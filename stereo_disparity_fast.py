1476d6eb3fefd0bc1104d3e56d79a0d44c00f76d
nicjonak <116019451+nicjonak@users.noreply.github.com>
2023-11-09 11:53:11 -0500

    Add files via upload

---
+++ stereo_disparity_fast.py
@@ -0,0 +1,69 @@
+import numpy as np
+from scipy.ndimage.filters import *
+
+def stereo_disparity_fast(Il, Ir, bbox, maxd):
+    """
+    Fast stereo correspondence algorithm.
+
+    This function computes a stereo disparity image from left stereo 
+    image Il and right stereo image Ir. Only disparity values within
+    the bounding box region (inclusive) are evaluated.
+
+    Parameters:
+    -----------
+    Il    - Left stereo image, m x n pixel np.array, greyscale.
+    Ir    - Right stereo image, m x n pixel np.array, greyscale.
+    bbox  - 2x2 np.array, bounding box, relative to left image, from top left
+            corner to bottom right corner (inclusive). (x, y) in columns.
+    maxd  - Integer, maximum disparity value; disparities must be within zero
+            to maxd inclusive (i.e., don't search beyond maxd).
+
+    Returns:
+    --------
+    Id  - Disparity image (map) as np.array, same size as Il.
+    """
+    # Hints:
+    #
+    #  - Loop over each image row, computing the local similarity measure, then
+    #    aggregate. At the border, you may replicate edge pixels, or just avoid
+    #    using values outside of the image.
+    #
+    #  - You may hard-code any parameters you require in this function.
+    #
+    #  - Use whatever window size you think might be suitable.
+    #
+    #  - Optimize for runtime AND for clarity.
+
+    #--- FILL ME IN ---
+
+    # Code goes here...
+    
+    ws = 5 #window size
+    Id=np.zeros_like(Il) #output image arrray
+    Irp=np.pad(Ir,ws,'constant',constant_values=0) #pad images for convolution with kernel size of ws
+    Ilp=np.pad(Il,ws,'constant',constant_values=0)
+
+    #loop through pixels in bounding box and check SAD score with other pixels in row from a disparity of -1 to maxd
+    for j in range(bbox[1][0]+ws, bbox[1][1]+ws):
+        for i in range(bbox[0][0]+ws, bbox[0][1]+ws):
+            BestD = 0
+            BestSAD = float('inf')
+            for disparity in range(-1,maxd):
+                if i-disparity-ws<0 or i-disparity+ws+1>Il.shape[1]: #check bounds
+                    continue
+                winL = Ilp[j-ws:j+ws+1, i-ws:i+ws+1]
+                winR = Irp[j-ws:j+ws+1, i-disparity-ws:i-disparity+ws+1]
+                SAD = np.sum(np.absolute(winL.flatten() - winR.flatten())) #compute SAD
+                if SAD < BestSAD:
+                    BestSAD = SAD
+                    BestD = abs(disparity)
+            Id[j-ws][i-ws] = BestD
+    
+
+    #------------------
+    correct = isinstance(Id, np.ndarray) and Id.shape == Il.shape
+
+    if not correct:
+        raise TypeError("Wrong type or size returned!")
+
+    return Id
